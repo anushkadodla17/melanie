ea264617a01ef58a5aae1b4141df6b2756a9f218
anushkadodla17 <anushkadodla17@gmail.com>
2026-01-12 22:47:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ if ingredients_list:
         ingredients_string+=fruit_chosen + ' '
     #st.write(ingredients_string)
 
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order')
     
